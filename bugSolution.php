823b6c220a8dc5e46e661445f093aba76f9ea017
BugHunter-X <askzenai@gmail.com>
2025-02-02 11:46:03 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,9 @@
+function myFunction() {
+  // Some code here...
+}
+
+// ... later in the code...
+
+if (function_exists('myFunction')) {
+  myFunction(); // Calling myFunction after checking if it's defined
+}
